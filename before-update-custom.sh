1cff6290d851dd3e611745f5d7901ec229dfd954
zhang33 <42689219+zhang33ya@users.noreply.github.com>
2025-09-29 00:21:18 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -4,6 +4,9 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# Use my luci 
+sed -i 's|src-git-full luci https://github.com/immortalwrt/luci.git;openwrt-21.02|src-git-full luci https://github.com/BeingFun/luci.git;openwrt-21.02|g' feeds.conf.default
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
